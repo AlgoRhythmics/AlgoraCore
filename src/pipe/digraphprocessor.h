212e40174a446cd880ce60690eddae02210f8e35
Kathrin Hanauer <kal@xaikal.org>
2016-01-06 22:18:22 +0100

    Added DiGraphProvider and DiGraphProcessor base class.
    Signed-off-by: Kathrin Hanauer <kal@xaikal.org>

---
+++ src/pipe/digraphprocessor.h
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C) 2013 - 2015 : Kathrin Hanauer
+ *
+ * This file is part of Algora.
+ *
+ * Algora is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Algora is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Algora.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact information:
+ *   http://algora.xaikal.org
+ */
+
+
+#ifndef GRAPHPROCESSOR_H
+#define GRAPHPROCESSOR_H
+
+namespace Algora {
+
+class DiGraph;
+
+class DiGraphProcessor
+{
+public:
+    DiGraphProcessor() { }
+    virtual ~DiGraphProcessor() { }
+
+    virtual void processGraph(const DiGraph *graph) = 0;
+};
+
+}
+
+#endif // GRAPHPROCESSOR_H
---
+++ src/pipe/pipe.pri
@@ -0,0 +1,7 @@
+message("pri file being processed: $$PWD")
+
+HEADERS += \ 
+    $$PWD/digraphprovider.h \
+    $$PWD/digraphprocessor.h
+
+SOURCES +=   
