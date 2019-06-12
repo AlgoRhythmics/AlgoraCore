54610681eafe002b60750c252b98bbd343590b0b
Kathrin Hanauer <kal@xaikal.org>
2019-06-12 16:06:40 +0200

    Make Observable pseudo-concurrent safe.
    Signed-off-by: Kathrin Hanauer <kal@xaikal.org>

--- src/observable.h
+++ src/observable.h
@@ -3,6 +3,8 @@
 
 #include <vector>
 #include <functional>
+#include <algorithm>
+#include <set>
 
 namespace Algora {
 
@@ -11,30 +13,60 @@ class Observable
 {
 public:
     typedef typename std::function<void(Ts...)> Notification;
-    Observable() { }
+    Observable() : notificationInProgress(false) { }
 
     Observable(const Observable &other) = default;
     Observable& operator=(const Observable &other) = default;
     Observable(Observable &&other) = default;
     Observable& operator=(Observable &&other) = default;
 
-    void addObserver(void *id, const Notification &fun) {
-        observers.push_back({id, fun});
+    void addObserver(void *id, const Notification &fun, bool delay = true) {
+        if (notificationInProgress && delay) {
+            delayedAdditions.push_back({id, fun});
+        } else {
+            observers.push_back({id, fun});
+        }
     }
 
-    void removeObserver(void *id) {
-        auto i = observers.begin();
-        while (i != observers.end()) {
-            if (id == i->first) {
-                i = observers.erase(i);
-            } else {
-                i++;
+    void removeObserver(void *id, bool delay = true) {
+        if (notificationInProgress && delay) {
+            delayedRemovals.insert(id);
+        } else {
+            auto i = observers.begin();
+            while (i != observers.end()) {
+                if (id == i->first) {
+                    i = observers.erase(i);
+                    break;
+                } else {
+                    i++;
+                }
             }
         }
     }
 
     void notifyObservers(Ts... ts) {
-       for (const auto &[_, fun] : observers) { fun(ts...); (void) _; }
+        // no concurrency support!
+        notificationInProgress = true;
+        for (const auto &[_, fun] : observers) { fun(ts...); (void) _; }
+        notificationInProgress = false;
+
+        if (!delayedAdditions.empty()) {
+            std::move(delayedAdditions.begin(), delayedAdditions.end(), std::back_inserter(observers));
+            delayedAdditions.clear();
+        }
+
+        if (!delayedRemovals.empty()) {
+            auto n = delayedRemovals.size();
+            for (auto i = observers.begin(), last = observers.end(); i != last && n > 0; ) {
+                if (delayedRemovals.find(i->first) != delayedRemovals.end()) {
+                    i = observers.erase(i);
+                    n--;
+                } else {
+                    i++;
+                }
+            }
+            delayedRemovals.clear();
+        }
     }
 
     bool hasObservers() const {
@@ -46,7 +78,10 @@ public:
     }
 
 private:
+    bool notificationInProgress;
     std::vector<std::pair<void*, Notification>> observers;
+    std::vector<std::pair<void*, Notification>> delayedAdditions;
+    std::set<void*> delayedRemovals;
 };
 
 }
